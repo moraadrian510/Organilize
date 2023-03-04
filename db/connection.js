088789a71ab414e14dae7fee51245acd67eb5114
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-04 14:12:57 -0800

    added sql files and connection

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+//require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Mercado510',
+    database: 'employee_tracker_db'
+});
+
+module.exports = db;
