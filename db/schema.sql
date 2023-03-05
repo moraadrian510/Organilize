bcc835e0ccd5d6ab577027411c4ef01c31558686
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-05 12:56:34 -0800

    fixed employee/manager_id refrence

--- db/schema.sql
+++ db/schema.sql
@@ -25,7 +25,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
+        REFERENCES employee(id)
+        ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,20 @@
 INSERT INTO department (name)
 VALUES 
-("Software Engineer"), 
-("Sales"), 
-("Finance"),
-("Legal");
+('Software Engineer'), 
+('Sales'), 
+('Finance'),
+('Legal');
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-("Senior Software Engineer",140000, 1),
-("Director of Sales", 150000, 2),
-("Cheif Financial Officer", 250000, 3),
-("Corporate Lawyer", 160000, 4);
+('Senior Software Engineer', 140000, 1),
+('Director of Sales', 150000, 2),
+('Chief Financial Officer', 250000, 3),
+('Corporate Lawyer', 160000, 4);
 
-
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-("Adrian Mora", "Mora", 1, 4),
-("John", "MILLER", 2, 3),
-("VICYORIA", "Mora", 3, 2),
-("Claudia ", "Diaz", 4, 1),;
+('Adrian', 'Mora', 1, 4),
+('John', 'MILLER', 2, 3),
+('Victoria', 'Mora', 3, 2),
+('Claudia', 'Diaz', 4, 1);
