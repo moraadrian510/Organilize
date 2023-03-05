f56dbe3cfd2ab8cf480332fa6e42d9ba7c3c6b6c
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-05 14:21:55 -0800

    removed sql ppsswrd

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Mercado510',
+    password: '',
     database: 'employee_tracker_db'
 });
 
